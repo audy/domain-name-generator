6a9e849a33b6ebed79a69fd7b962be48b746b309
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-01-17 20:26:51 -0500

    feh

--- tld.py
+++ tld.py
@@ -1,7 +1,30 @@
-words = [i.strip().lower() for i in open('/usr/share/dict/words').readlines()]
-tlds = [i.split()[0].strip().lower() for i in open('tlds.txt').readlines()]
+from os import system as sys
+from random import choice
 
-for word in words:
-  for tld in tlds:
-    if word.endswith(tld):
-      print '%s.%s' % (word.rstrip(tld), tld)
+class DomainGetter:
+  
+  def __init__(self):
+    self.words = [i.strip().lower() for i in open('/usr/share/dict/words').readlines()]
+    self.tlds = [i.split()[0].strip().lower() for i in open('tlds.txt').readlines()]
+  
+  def __iter__(self):
+    while len(self.words) > 1:
+      
+      word = choice(self.words)
+      self.words.remove(word)
+      
+      for tld in self.tlds:
+        if word.endswith(tld):
+          yield '%s.%s' % (word.rstrip(tld), tld)
+        else:
+          yield False
+
+def taken(domain):
+  a = sys('nslookup %s' % domain)
+  quit()
+          
+domains = DomainGetter()
+
+for domain in domains:
+  if domain:
+    print domain, taken(domain)
