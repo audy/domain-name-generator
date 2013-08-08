230fdcf61a85da75779cf6e8e87aa733d13de727
audy <harekrishna@gmail.com>
2013-08-08 16:48:52 -0400

    trailing whitespace

--- tld.py
+++ tld.py
@@ -2,17 +2,17 @@ from os import system as sys
 from random import choice
 
 class DomainGetter:
-  
+
   def __init__(self):
     self.words = [i.strip().lower() for i in open('/usr/share/dict/words').readlines()]
     self.tlds = [i.split()[0].strip().lower() for i in open('tlds.txt').readlines()]
-  
+
   def __iter__(self):
     while len(self.words) > 1:
-      
+
       word = choice(self.words)
       self.words.remove(word)
-      
+
       for tld in self.tlds:
         if word.endswith(tld):
           yield '%s.%s' % (word.rstrip(tld), tld)
