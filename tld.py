ecb969f28d401575f8cf7b281d413e3e4cbf5bb5
Austin Davis-Richardson <harekrishna@gmail.com>
2017-08-22 11:24:33 -0700

    all generators

--- tld.py
+++ tld.py
@@ -30,24 +30,18 @@ def parse_arguments():
     return p.parse_args()
 
 
-def get_words(words_file, min_length=1):
+def iter_words(handle):
     ''' iterate over list of words in text file '''
 
-    words = []
-
-    with open(words_file) as handle:
-        for word in handle:
-            word = word.strip().lower()
-            if len(word) > min_length:
-                words.append(word)
-
-    return words
+    return (
+        word.strip().lower()
+        for word
+        in handle
+    )
 
 
 def get_tlds(tlds_file):
     ''' iterate over list of tlds in text file '''
-
-    tlds = []
 
     with open(tlds_file) as handle:
 
@@ -57,8 +51,7 @@ def get_tlds(tlds_file):
         ]
 
 
-
-def get_domains(words, tlds):
+def iter_domains(words, tlds):
     ''' list domains made from words and tlds '''
 
     return (
@@ -67,6 +60,7 @@ def get_domains(words, tlds):
         for word in words
         if word.endswith(tld)
     )
+
 
 def l33tify(domain):
     ''' Produce 1337 versions of words '''
@@ -95,25 +89,19 @@ def l33tify(domain):
 if __name__ == '__main__':
     args = parse_arguments()
 
-    words = get_words(args.words_file)
 
     if not args.tlds:
         tlds = get_tlds(args.tlds_file)
     else:
         tlds = args.tlds.split(',')
 
-    domains = get_domains(words, tlds)
+    with open(args.words_file) as handle:
+        processed_domains = (
+            l33tify(domain) if args.leet else domain
+            for domain
+            in iter_domains(iter_words(handle), tlds)
+            if len(domain) in range(args.min_size, args.max_size)
+        )
 
-    if args.leet:
-        domains = ( l33tify(domain) for domain in domains )
-
-    # length filter
-    domains = (
-        domain
-        for domain
-        in domains
-        if len(domain) in range(args.min_size, args.max_size)
-    )
-
-    for domain in domains:
-        print(domain)
+        for domain in processed_domains:
+            print(domain)
